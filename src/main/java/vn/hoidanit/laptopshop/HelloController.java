b8a5300f2933da6b3e4f549fae400cfb1e483fb5
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-09-25 16:44:50 +0700

    Spring dev tools and demo route

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -10,4 +10,14 @@ public class HelloController {
     public String index() {
         return "Hello World with Spring Boot!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only User can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin can access this page";
+    }
 }
