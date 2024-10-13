7498a3a7f32676bf5a0a0dd06b79e730fcc8b72a
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-10-13 22:22:42 +0700

    Design User Detail Page

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -40,6 +41,12 @@ public class UserController {
         List<User> users = this.userService.getAllUsers();
         model.addAttribute("users", users);
         return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        return "admin/user/show";
     }
 
     @RequestMapping("/admin/user/create") // GET
