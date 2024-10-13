2d52905d4d34ecdeef283c1a7fc7962684ab1c93
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-10-13 10:54:08 +0700

    Define repository query

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.User;
@@ -14,8 +16,17 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
     public User handleSaveUser(User user) {
         User saveUser = this.userRepository.save(user);
+        System.out.println(saveUser);
         return saveUser;
     }
 }
