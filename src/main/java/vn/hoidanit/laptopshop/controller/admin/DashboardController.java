c587bb01c6f55d3713640686cef0a5f1bf65311d
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-10-16 21:38:26 +0700

    Design Form Admin Dashboard from Template

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/DashboardController.java
@@ -0,0 +1,21 @@
+package vn.hoidanit.laptopshop.controller.admin;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+    
+    @GetMapping("/admin")
+    public String getDashboard() {
+        return "admin/dashboard/show";
+    }
+}
