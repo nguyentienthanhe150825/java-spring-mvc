e771f0ecbb4270a34b2ac6171fd35af7c5ef7616
Tien Thanh <nguyenthanh2542001@gmail.com>
2024-10-30 17:11:13 +0700

    LoadUserByUsername - CustomUserDetailsService - SecurityConfiguration

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -2,9 +2,17 @@ package vn.hoidanit.laptopshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -14,5 +22,37 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
+    }
+
+    // @Bean
+    // public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder,
+    //         UserDetailsService userDetailsService) throws Exception {
+    //     AuthenticationManagerBuilder authenticationManagerBuilder = http
+    //             .getSharedObject(AuthenticationManagerBuilder.class);
+    //     authenticationManagerBuilder
+    //             .userDetailsService(userDetailsService)
+    //             .passwordEncoder(passwordEncoder);
+    //     return authenticationManagerBuilder.build();
+    // }
+
+    //https://stackoverflow.com/questions/43007763/spring-security-encoded-password-gives-me-bad-credentials
+    @Bean
+    public DaoAuthenticationProvider authProvider(
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService) {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        //authProvider.setHideUserNotFoundExceptions(false);   //Ném ra lỗi ở class CustomUserDetailsService
+
+        return authProvider;
+    }
+
+
 
 }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -30,7 +30,7 @@ public class UserService {
     }
 
     public List<User> getAllUsersByEmail(String email) {
-        return this.userRepository.findByEmail(email);
+        return this.userRepository.findOneByEmail(email);
     }
 
     public User handleSaveUser(User user) {
@@ -61,6 +61,10 @@ public class UserService {
     //spring jps check email exist
     public boolean checkEmailExist(String email) {
         return this.userRepository.existsByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
     }
 
 }
